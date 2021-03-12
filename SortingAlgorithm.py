62e773c016fa4d94fa1cecd18d01de3941f032d1
Kaiyi <kaiyiling3@gmail.com>
2021-03-12 11:38:18 +0800

    first init

---
+++ SortingAlgorithm.py
@@ -0,0 +1,27 @@
+"""
+Sorting: change the postion of the array
+you can definitely using Brute Force! 
+But, you should always consider is there any clever way!
+"""
+
+# BubbleSort
+
+# MergeSort
+
+# QuickSort 
+def partion(array, low, high):
+    
+    pivot = array[-1] 
+
+def quicksort(array):
+    """Divide amd Conquer"""
+
+    if len(array) == 1:
+        return array
+        
+    else:
+        quicksort()
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
+
