4c01ab1eaec83822b0c344c8cd98b5d70be953d5
karry <kaiyiling3@gmail.com>
2024-03-04 10:15:40 +0800

    passed

--- TwoPointers/easy_125_validate_palindrome.py
+++ TwoPointers/easy_125_validate_palindrome.py
@@ -37,9 +37,16 @@ class Solution:
         p1 = 0
         p2 = len(s) - 1
         while p1 < p2:
-            if s[p1] == s[p2]:
+            if s[p1].lower() == s[p2].lower():
                 p1 += 1
                 p2 -= 1
+            elif not s[p1].isalnum():
+                p1 += 1
+            elif not s[p2].isalnum():
+                p2 -= 1
+                
             else:
                 return False
         return True
+    
+# Passed
